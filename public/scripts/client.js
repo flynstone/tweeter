a4eb3c792d6284c2563cedf1546b2c63482a0b17
Julien <flynstone@live.ca>
2021-12-16 19:11:11 -0500

    Commit dynamic tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,67 +6,90 @@
 
 // Fake data taken from initial-tweets.json
 
-
-const createTimeStamp = (stamp) => {
-  let tweets = timeago.format(data.created_at);
-  for (let tweet of stamp) {
-    tweets += `<span class="timestamp" datetime="${data.created_at}">${tweet}</span>`;
-  }
-  return tweets;
-}
-
-
-
 const createTweetElement = (data) => {
   const $tweet = $(`
-    <article class="tweet">              
-      <header>       
-        <div class="inline">              
-          <img src="${data.user.avatars}" alt="face">        
-          <label class="title">${data.user.name}</label>       
-        </div>     
-        <label><b>${data.user.handle}</b></label>    
-      </header>
-
-      <div class="tweet-content">
-        <textarea>${data.content.text}</textarea>
-      </div>
-
-      <footer>
-        <span class="timeago">${timeago.format(data.created_at)}</span>
-
-        <span class="icon-group">
-          <i class="fa fa-flag" aria-hidden="true"></i>
-          <i class="fa fa-retweet" aria-hidden="true"></i>
-          <i class="fa fa-heart" aria-hidden="true"></i>
-        </span>
-      </footer>
-    </article>
-  `);
-
+  <article class="tweet">              
+    <header>       
+      <div class="inline">              
+        <img src="${data.user.avatars}" alt="face">        
+        <label class="title">${data.user.name}</label>       
+      </div>     
+      <label><b>${data.user.handle}</b></label>    
+    </header>
+    <div class="tweet-content">
+      <textarea>${data.content.text}</textarea>
+    </div>
+    <footer>
+      <span class="timeago">${timeago.format(data.created_at)}</span>
+      <span class="icon-group">
+        <i class="fa fa-flag" aria-hidden="true"></i>
+        <i class="fa fa-retweet" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+      </span>
+    </footer>
+  </article>
+`);
   return $tweet;
 }
 
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 
 const renderTweets = (tweets) => {
-  for (let tweet of tweets) {
-    $('#tweets-container').append(createTweetElement(tweet));
+  tweets = tweets.reverse();
+  for (const tweet of tweets) {
+    $('.tweets-container').append(createTweetElement(tweet));
   }
 }
 
 
-const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
-    .then((tweets) => {
-      renderTweets(tweets);
-    })
-    .catch((err) => {
-      throw err;
-    })
+const loadTweets = function () {
+  $.get("/tweets/", function($tweets) {
+    renderTweets($tweets);
+  });
 };
 
-loadTweets();
+$(document).ready(function() {
+  loadTweets()
+});
 
+$(document).ready(function () {
+  $('.add-tweet').submit(function (event) {
+    event.preventDefault();
+    const $form = $(this);
+    const $postLength = $form.children('textarea').val().length;
+
+    // Front End Validity Checks
+    // Check for an empty field
+    if ($postLength === 0) {
+      $form.append($(`<p id='error' class='red'>Text area is empty</p>`))
+      return;
+    }
+    // Checks the length
+    if ($postLength > 140) {
+      $form.append($(`<p id='error' class='red'>Tweet is more than 140 characters long</p>`))
+      return;
+    }
+
+    // Ajax insetrs a new post
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      success: function (data) {
+        // Insetrs new tweet after the tweet form
+        location.reload();
+      }
+    });
+    // Clears the text area once the tweet is posted
+    $form.children('textarea').val('');
+  });
+});
+/*
 $(document).ready(function () {
   $('form.newTweet').on('submit', function (event) {
     event.preventDefault();
@@ -81,6 +104,17 @@ $(document).ready(function () {
       throw err;
     })
   });
-  renderTweets(data);
-});
+
+});*/
+
+/*
+$(document).ready(function() {
+  console.log('doc is ready')
+
+  $('form.tweet-submit').on('submit', submitTweetPost);
+
+  $('.writeTweet').on('click', function() {
+    $('.new-tweet').toggle(200);
+  });
+});*/
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('textarea').on('input', function (event) {
     let val = $(this).val().length
-    let $counter= $(this).parent('form').find('.counter')
+    let $counter = $(this).parent('form').find('.counter')
     counting($counter, val)
   })
-})
+});
+
 
 function counting($counter, val) {
-  let count = 140 - val
-  $counter.text(count)
-  $counter.css('color', 'floralwhite')
+  let count = 140 - val;
+  $counter.text(count);
+  $counter.css('color', 'floralwhite');
   if (count < 0) {
-    $counter.css('color', 'red')
+    $counter.css('color', 'red');
   }
 }
